08788f05674297ab204b824984e4d69d54997f55
sulemanmasih-codora <suleman.bhatti@codora.io>
2024-01-03 16:33:03 +0500

    wip

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -20,7 +20,7 @@ provider "github" {
 
 }
 
-module "fetch&put_secret" {
+module "fetch_secret" {
   source            = "../../modules/secret_fetch"
   vault_id          = var.vault_id
   uuid_id           = var.uuid_id
