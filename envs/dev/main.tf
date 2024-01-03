241e523f59369d3afcac0a03ab6f1a1d9de5ed1a
sulemanmasih-codora <suleman.bhatti@codora.io>
2024-01-03 17:59:22 +0500

    wip

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -19,10 +19,10 @@ provider "github" {
   token = var.gh_token
 }
 
-# module "fetch_secret" {
-#   source            = "../../modules/secret_fetch"
-#   vault_id          = var.vault_id
-#   uuid_id           = var.uuid_id
-#   github_repository = var.gh_repository
-#   environment       = var.environment
-# }
+module "fetch_secret" {
+  source            = "../../modules/secret_fetch"
+  vault_id          = var.vault_id
+  uuid_id           = var.uuid_id
+  github_repository = var.gh_repository
+  environment       = var.environment
+}
