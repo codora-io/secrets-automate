2576fb709198f746f12b034f955da9c7d6815d3a
sulemanmasih-codora <suleman.bhatti@codora.io>
2024-01-12 14:53:25 +0500

    wip

--- envs/lexim-dev/1pass.tf
+++ envs/lexim-dev/1pass.tf
@@ -2,7 +2,7 @@ module "fetch_secret" {
   source            = "../../modules/secret_fetch"
   vault_id          = var.vault_id
   uuid_id           = var.uuid_id
-  github_repository = "lexim-aws-infra"
+  github_repository = "codora-io/lexim-aws-infra"
   environment       = var.environment
 }
 
